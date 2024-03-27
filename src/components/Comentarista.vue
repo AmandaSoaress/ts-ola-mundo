5268b0f1315094074747ea96e8cffaa3f0870007
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2024-03-27 12:22:05 -0300

    first committee

---
+++ src/components/Comentarista.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="comentar">Comentar</button>
+    <p>
+      {{ comentario }}
+    </p>
+  </div>
+</template>
+<script  lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Comentarista extends Vue {
+
+    private comentario = ''
+    private comentar () : void{
+       this.comentario = 'Um mega comentário bem bacana!' 
+    } 
+}
+</script>
